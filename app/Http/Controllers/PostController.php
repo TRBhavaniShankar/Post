be59458ba0bcb01a6fe1d16b4fa90da891a6db41
TRBhavaniShankar <tr.bhavanishankar93@gmail.com>
2021-07-28 16:35:12 +0530

    WIP

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     //
     public function index(){
-        return view('posts.index');
+
+        $posts = Post::paginate(2);
+
+        return view('posts.index',[
+            'posts' => $posts
+        ]);
     }
 
     public function create(Request $request){
         
+        $this->validate($request,[
+           'body' => 'required'
+        ]);
+
+        // Post::create([
+        //     'user_id' => auth()->id(),
+        //     'body' => $request->body
+        // ]);
+
+        $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return back();
+
     }
 }
